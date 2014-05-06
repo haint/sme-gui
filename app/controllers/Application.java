f20d40ef74dcdf6686b15b6f1cdc6af60896175d
haint <haithanh0809@gmail.com>
2014-05-06 19:02:06 +0700

    Fork AdminFlare themes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,11 +2,16 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.*;
 
 
 public class Application extends Controller {
 
   public static Result index() {
     return redirect(controllers.routes.Wizard.step(1));
+  }
+  
+  public static Result demo() {
+    return ok(demo.render());
   }
 }
