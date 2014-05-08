054bc7c12695e9b5857ec7630c746e0b918af921
haint <haithanh0809@gmail.com>
2014-05-08 18:46:14 +0700

    Mock ui for VM management

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,34 @@
 package controllers;
 
+import play.Routes;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.*;
 
 
 public class Application extends Controller {
 
   public static Result index() {
-    return redirect(controllers.routes.Wizard.step(1));
+    return ok(main.render());
+  }
+  
+  public static Result signin() {
+    return ok(signin.render());
+  }
+  
+  public static Result modal(int type) {
+    switch (type) {
+    //create new vm
+    case 1:
+      return ok(createMachineModal.render());
+    default:
+      return status(404).as("Unsupported modal type " + type);
+    }
+  }
+  
+  public static Result javascriptRoutes() {
+    response().setContentType("text/javascript");
+    return ok(Routes.javascriptRouter("router", 
+        routes.javascript.Application.modal()));
   }
 }
